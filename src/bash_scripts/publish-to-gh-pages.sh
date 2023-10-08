63ad9a5970c9d9b1ed32cc01b6d13242a070af60
lpupp <lucagaegauf@gmx.ch>
2023-10-08 14:07:12 +0200

    script to publish pages

--- src/bash_scripts/publish-to-gh-pages.sh
+++ src/bash_scripts/publish-to-gh-pages.sh
@@ -2,12 +2,14 @@ set -e
 
 GITHUB_USERNAME=$1
 REPO_NAME=$2
-WEBPAGE_DIR=$3
-BACKTEST_DIR="$4/$5"
-BACKTEST_PERF_DATA=$6
-BACKTEST_COMP_DATA=$7
+WEBPAGE_INDEX=$3
+BACKTEST_PERF_DATA=$4
+BACKTEST_COMP_DATA=$5
+OUTPUT_PATH=$6
+
+WEBPAGE_DIR=$(dirname $WEBPAGE_INDEX)
+BACKTEST_DIR=$(dirname $BACKTEST_PERF_DATA)
 GH_PAGES_URL="https://$GITHUB_USERNAME.github.io/$REPO_NAME"
-OUTPUT_PATH=$8
 
 # remove the directory if it exists and start from scratch
 rm -rf ./dist
@@ -15,21 +17,20 @@ mkdir dist
 mkdir dist/data
 
 # copy everything to new repo
-BACKTEST_DIR='data/portfolios'
 cp -r $WEBPAGE_DIR/* dist -u
 cp -r $BACKTEST_DIR/*.csv dist/data -u
 cd dist
 
-# copy index.html
 # but for this to work, the relative paths in the html need to change because dist is root
 sed -i 's|../../data/portfolios/|data/|g' js/index.js
 
 # publish new github repo
-git init --initial-branch=main
-git add -A
-git commit -m 'Deploy to GH pages'
-
-git push -f git@github.com:$GITHUB_USERNAME/$REPO_NAME.git main:gh-pages
+git init
+git remote add origin ..
+git checkout -b gh-pages
+git add .
+git commit -m "Add dist directory content for GitHub Pages"
+git push origin gh-pages
 
 # go back and delete everything
 # (this is optional, you can also keep your folder locally)
@@ -38,7 +39,9 @@ cd ..
 rm -rf ./dist 
 
 # create a timestamped textfile telling us when the latest publication happened
-date +"%m/%d/%Y %H:%M:%S $HOSTNAME" > "./src/doc/gh_publication_date_ddmmyy.txt"
+date +"%m/%d/%Y %H:%M:%S $HOSTNAME" > "$OUTPUT_PATH"
 
 # open webpage
 python -m webbrowser $GH_PAGES_URL
+
+echo "The page may not display! Go to https://github.com/$GITHUB_USERNAME/$REPO_NAME/settings/pages and select gh-pages branch. Wait for webpage to build and then reload."
